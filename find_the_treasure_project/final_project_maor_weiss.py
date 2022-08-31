b30591f6bb1e3247affbd68a67f6b854717f7ad9
maor weiss <maorweissb@gmail.com>
2022-09-01 01:38:27 +0300

    Challenge - Final rejects#02

--- find_the_treasure_project/final_project_maor_weiss.py
+++ find_the_treasure_project/final_project_maor_weiss.py
@@ -1,6 +1,5 @@
 from random import randint
 from os.path import exists
-import pandas as pd
 import csv
 
 """
@@ -26,7 +25,6 @@ Step2:
 steps_to_win = 0
 position = 0
 length_of_characters = 0
-lower_score = 0
 
 
 def create_new_game_files():
@@ -67,80 +65,96 @@ def get_position(num_of_steps, length_of_characters):
 
 # The challenge
 # If the player guessed under ten guesses, record the name and the score to a CSV file table
-def challenge(steps_to_win):
+def challenge(steps_to_win, num_of_scores):
     print()  # For space
     player_name = input('Congrads! You\'ve guessed won the game! What is your name?  ')
     with open('score_file.csv', 'a', newline='') as players_file:
         write_payer = csv.writer(players_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         write_payer.writerow([player_name, steps_to_win])
 
-    # # Sorting the rows by the 'steps_to_win score'
-    # csvData = pandasForSortingCSV.read_csv('score_file.csv')
-    # # csvData.sort_values(by=' Score')
-    # new_csv = csvData.sort_values(by=[" Score"], ascending=[False])
-    # new_csv.to_csv('score_file.csv')
+    # Creating new_scores_table by list
+    score_file = open('score_file.csv', 'r+')
+    score_list = list(csv.reader(score_file))
 
-    return 0  # Lower_score
+    # Sorting the rows by the 'steps_to_win score'
+    score_list.sort(key=lambda x: x[1])
+
+    # If there are more than 10 scores - Delete the last row, which is the lower score
+    if num_of_scores > 10:
+        score_list.pop()
+    score_file.close()
+
+    new_scores_table = open('score_file.csv', 'w+', newline='')
+
+    # writing the new_scores_table into the score file
+    with new_scores_table:
+        score = csv.writer(new_scores_table)
+        score.writerows(score_list)
+    new_scores_table.close()
 
 
 def get_score_file_parameters():
     score_file = open('score_file.csv')
-    reader = csv.reader(score_file)
-    num_of_scores = len(list(reader)) - 1  # First row doesn't count as a score
-    score_list = list(csv.reader(score_file, delimiter=","))
-    print(score_list)
+    score_list = list(csv.reader(score_file))
+    num_of_scores = len(score_list) - 1  # First row doesn't count as a score
+    if num_of_scores > 0:
+        lower_score = int(score_list[-1][1])
+    else:
+        lower_score = 0  # If the CSV is empty
     score_file.close()
 
-    return num_of_scores
+    return num_of_scores, lower_score
 
 
 print('Welcome to the "find the treasure" game ')
 # Step1
 create_new_game_files()
-num_of_scores = get_score_file_parameters()
+num_of_scores, lower_score = get_score_file_parameters()
 
 # Step2
-# with open("find_the_treasure_file.txt", 'r') as game_file:
-#     while True:
-#         direction = input("Where do you want to move? [1- forward 2-backwards] ")
-#
-#         if not direction in '12':
-#             print("again...")
-#             continue  # If the user didn't use '1' or '2', ask an input again...
-#
-#         try:  # For characters which are not numbers
-#             num_of_steps = int(input("How many characters? "))
-#         except ValueError:
-#             print("again...")
-#             continue
-#
-#         # For efficiency, if the length is not equal to zero, the variable has been calculated
-#         if length_of_characters == 0:
-#             data = game_file.read()
-#             length_of_characters = len(data)
-#
-#         if not num_of_steps >= 0:
-#             print("Pls Write a positive number for the characters' steps")
-#             continue
-#
-#         if direction == '1':
-#             position = get_position(num_of_steps + position, length_of_characters)
-#
-#         else:  # direction == '2'
-#             position = get_position((num_of_steps * -1) + position, length_of_characters)
-#
-#         steps_to_win += 1
-#         game_file.seek(position)
-#         chr = game_file.readline(1)
-#
-#         print(f'You hit the character “{chr}”. ')
-#         if chr in 'TREASURE':
-#             break
-#         else:
-#             print('… again … until hit one of the “TREASURE” letters…')
-#
-# print(f'It took you {steps_to_win} steps to get there')
+with open("find_the_treasure_file.txt", 'r') as game_file:
+    while True:
+        direction = input("Where do you want to move? [1- forward 2-backwards] ")
 
-# steps_to_win = 14  # Todo - Remove after debugging
-# if steps_to_win >= lower_score or num_of_scores <= 10:
-#     lower_score = challenge(steps_to_win)
+        if not direction in '12':
+            print("again...")
+            continue  # If the user didn't use '1' or '2', ask an input again...
+
+        try:  # For characters which are not numbers
+            num_of_steps = int(input("How many characters? "))
+        except ValueError:
+            print("again...")
+            continue
+
+        # For efficiency, if the length is not equal to zero, the variable has been calculated
+        if length_of_characters == 0:
+            data = game_file.read()
+            length_of_characters = len(data)
+
+        if not num_of_steps >= 0:
+            print("Pls Write a positive number for the characters' steps")
+            continue
+
+        if direction == '1':
+            position = get_position(num_of_steps + position, length_of_characters)
+
+        else:  # direction == '2'
+            position = get_position((num_of_steps * -1) + position, length_of_characters)
+
+        steps_to_win += 1
+        game_file.seek(position)
+        chr = game_file.readline(1)
+
+        print(f'You hit the character “{chr}”. ')
+        if chr in 'TREASURE':
+            break
+        else:
+            print('… again … until hit one of the “TREASURE” letters…')
+
+print(f'It took you {steps_to_win} steps to get there')
+
+if steps_to_win <= lower_score or num_of_scores <= 10:
+    num_of_scores += 1
+    challenge(steps_to_win, num_of_scores)
+
+print('Thank you for playing!')
